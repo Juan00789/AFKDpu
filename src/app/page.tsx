55d18ff8960ddadebb92b6e128cd3fe09fcd9fb8
Juan Ismael Alcantara Rodriguez <afkmulticervices@gmail.com>
2025-07-19 20:08:35 +0000

    elimina el formulario de consulta acuerdate que tenemos ya la base solid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Timeline from '@/components/sections/Timeline';
 import Services from '@/components/sections/Services';
 import GoodNews from '@/components/sections/GoodNews';
 import Manuals from '@/components/sections/Manuals';
-import Concierge from '@/components/sections/Concierge';
 
 export default function Home() {
   return (
@@ -15,7 +14,6 @@ export default function Home() {
       <main className="flex-1">
         <Help />
         <Services />
-        <Concierge />
         <Stories />
         <Timeline />
         <Manuals />
